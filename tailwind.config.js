dc8be26e6c9ea8d625ffe5e9a88bc90dfe5220a1
hardiksharma23 <sharmahardik270@gmail.com>
2024-02-09 16:10:17 +0530

    Home page feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,29 @@
 module.exports = {
   content: ["./templates/*.html"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+        guirella: ["Protest Guerrilla", "sans-serif"],
+        pinscher: [ 'SH Pinscher', 'sans-serif']
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+        purple: "#6b44ff",
+        black: "#0b0813"
+      }
+    },
+
+    },
   plugins: [],
 }
 
